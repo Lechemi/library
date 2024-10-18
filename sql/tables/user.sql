37eecdb6417b2c50cdcc8c3223ba2741892f2d7b
Lechemi <micheleceroni0@gmail.com>
2024-10-18 16:04:30 +0200

    created user_type. Now user has an user_type column

--- sql/tables/user.sql
+++ sql/tables/user.sql
@@ -4,5 +4,6 @@ CREATE TABLE "user"
     email      VARCHAR(100) NOT NULL UNIQUE CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
     password   VARCHAR(100) NOT NULL CHECK (LENGTH(password) > 5),
     first_name VARCHAR(100) NOT NULL CHECK (first_name ~* '^.+$'),
-    last_name  VARCHAR(100) NOT NULL CHECK (last_name ~* '^.+$')
+    last_name  VARCHAR(100) NOT NULL CHECK (last_name ~* '^.+$'),
+    type       user_type    not null
 );
