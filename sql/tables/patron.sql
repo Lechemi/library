869de267ff77f4ca7d73575a5f47e1e3a7d4c05f
Lechemi <micheleceroni0@gmail.com>
2024-10-19 16:26:58 +0200

    tax_code has to consist of 16 alphanumeric characters

--- sql/tables/patron.sql
+++ sql/tables/patron.sql
@@ -1,7 +1,11 @@
 CREATE TABLE patron
 (
     "user"   UUID PRIMARY KEY REFERENCES "user" (id),
-    tax_code char(16) NOT NULL CHECK (tax_code ~* '^.+$'),
-    n_delays SMALLINT     NOT NULL DEFAULT 0,
-    category VARCHAR(50)  NOT NULL REFERENCES patron_category ("name")
+    tax_code CHAR(16)    NOT NULL CHECK (tax_code ~* '^.+$'),
+    n_delays SMALLINT    NOT NULL DEFAULT 0,
+    category VARCHAR(50) NOT NULL REFERENCES patron_category ("name")
 );
+
+ALTER TABLE patron
+    ADD CONSTRAINT check_alphanumeric
+        CHECK (patron.tax_code ~ '^[A-Za-z0-9]{16}$');
