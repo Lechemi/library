fdf3b6b73ad46baa1254d376bf9db35f190dae4e
Lechemi <micheleceroni0@gmail.com>
2024-10-19 16:19:33 +0200

    added 'alive' attribute in author table

--- sql/tables/author.sql
+++ sql/tables/author.sql
@@ -4,6 +4,7 @@ CREATE TABLE author
     first_name VARCHAR(100) NOT NULL CHECK (first_name ~* '^.+$'),
     last_name  VARCHAR(100) NOT NULL CHECK (last_name ~* '^.+$'),
     bio        TEXT         NOT NULL CHECK (bio ~* '^.+$'),
-    birth_date DATE         NOT NULL,
-    death_date DATE
+    birth_date DATE,
+    death_date DATE,
+    alive      BOOLEAN      NOT NULL
 );
