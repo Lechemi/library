fcacc2035074eb5392f03b5295544d49ca77a948
Lechemi <micheleceroni0@gmail.com>
2024-10-22 16:47:42 +0200

    formatting

--- sql/procedures/user.sql
+++ sql/procedures/user.sql
@@ -1,24 +1,24 @@
-create procedure create_patron(
-    _email "user".email%type,
-    _password "user".password%type,
-    _first_name "user".first_name%type,
-    _last_name "user".last_name%type,
-    _tax_code patron.tax_code%type,
-    _category patron.category%type
+CREATE PROCEDURE create_patron(
+    _email "user".EMAIL%TYPE,
+    _password "user".PASSWORD%TYPE,
+    _first_name "user".FIRST_NAME%TYPE,
+    _last_name "user".LAST_NAME%TYPE,
+    _tax_code patron.TAX_CODE%TYPE,
+    _category patron.CATEGORY%TYPE
 )
-language plpgsql
-as
+    LANGUAGE plpgsql
+AS
 $$
-declare
-    _id uuid;
-begin
+DECLARE
+    _id UUID;
+BEGIN
 
-    insert into "user" (email, password, first_name, last_name, type)
-    values (_email, _password, _first_name, _last_name, 'patron')
-    returning id into _id;
+    INSERT INTO "user" (email, password, first_name, last_name, type)
+    VALUES (_email, _password, _first_name, _last_name, 'patron')
+    RETURNING id INTO _id;
 
-    insert into patron ("user", tax_code, category)
-    values (_id, _tax_code, _category);
+    INSERT INTO patron ("user", tax_code, category)
+    VALUES (_id, _tax_code, _category);
 
-end;
+END;
 $$;
