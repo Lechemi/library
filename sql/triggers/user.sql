27dbb14c28a96999f7df519026908cba8d749623
Lechemi <micheleceroni0@gmail.com>
2024-10-24 15:21:03 +0200

    user: enforce removal policy

--- sql/triggers/user.sql
+++ sql/triggers/user.sql
@@ -48,6 +48,46 @@ EXECUTE PROCEDURE user_deny_unmodifiable_fields_update();
 
 
 
+/**
+  'removed' field can only go from FALSE to TRUE.
+  Before setting 'removed' field to TRUE for a patron, the corresponding patron
+  record must already be removed.
+ */
+CREATE OR REPLACE FUNCTION user_enforce_removal_policy() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _patron_is_removed BOOLEAN;
+BEGIN
+    IF new.removed IS DISTINCT FROM old.removed THEN
+
+        IF new.removed IS FALSE THEN
+            RAISE EXCEPTION 'Removed users cannot be restored.';
+        END IF;
+
+        SELECT patron.removed
+        FROM patron
+        WHERE new.id = patron."user"
+        INTO _patron_is_removed;
+
+        IF NOT _patron_is_removed THEN
+            RAISE EXCEPTION 'Before removing a patron user, the corresponding patron record must already be removed.';
+        END IF;
+
+    END IF;
+
+    RETURN new;
+END;
+$$;
+
+CREATE TRIGGER bu_user_enforce_removal_policy
+    BEFORE UPDATE
+    ON "user"
+    FOR EACH ROW
+EXECUTE PROCEDURE user_enforce_removal_policy();
+
+
 
 -- Deny deletion of records
 CREATE TRIGGER bd_user_deny_deletion
