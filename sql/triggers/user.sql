d548b781e5f0d787a415237ecd3d52d1d0c4b03b
Lechemi <micheleceroni0@gmail.com>
2024-10-24 14:56:54 +0200

    user: deny modification of fields id, email or type

--- sql/triggers/user.sql
+++ sql/triggers/user.sql
@@ -18,7 +18,33 @@ EXECUTE PROCEDURE set_removed_to_false();
 
 
 
+-- Deny modification of fields id, email or type
+CREATE OR REPLACE FUNCTION user_deny_unmodifiable_fields_update() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF new.id != old.id THEN
+        RAISE EXCEPTION 'Cannot modify id field.';
+    END IF;
 
+    IF new.email != old.email THEN
+        RAISE EXCEPTION 'Cannot modify email field.';
+    END IF;
+
+    IF new.type != old.type THEN
+        RAISE EXCEPTION 'Cannot modify type field.';
+    END IF;
+
+    RETURN new;
+END;
+$$;
+
+CREATE TRIGGER bu_user_deny_unmodifiable_fields_update
+    BEFORE UPDATE
+    ON "user"
+    FOR EACH ROW
+EXECUTE PROCEDURE user_deny_unmodifiable_fields_update();
 
 
 
