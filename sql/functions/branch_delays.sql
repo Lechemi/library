5c8a20e8405c91cda68d84808fd95689415dd40b
Lechemi <micheleceroni0@gmail.com>
2024-10-29 18:18:50 +0100

    branch stats: delay stats for each branch

---
+++ sql/functions/branch_delays.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION delays(my_branch branch.ID%TYPE)
+    RETURNS TABLE
+            (
+                PATRON patron.USER%TYPE,
+                BOOK   book.ISBN%TYPE,
+                COPY   book_copy.ID%TYPE
+            )
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT loan.patron AS patron, bc.book AS book, bc.id AS copy
+        FROM loan
+                 INNER JOIN library.book_copy bc ON bc.id = loan.copy
+        WHERE returned IS NULL
+          AND bc.branch = my_branch
+          AND NOW() > loan.due;
+END;
+$$;
