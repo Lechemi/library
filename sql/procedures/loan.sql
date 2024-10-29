07e1153cd9b513aa99fd11706a2e42a951b5994d
Lechemi <micheleceroni0@gmail.com>
2024-10-29 15:51:42 +0100

    procedure make_loan

---
+++ sql/procedures/loan.sql
@@ -0,0 +1,53 @@
+CREATE OR REPLACE PROCEDURE make_loan(
+    _book book.ISBN%TYPE,
+    _patron patron.USER%TYPE,
+    _preferred_branches INT[] DEFAULT NULL
+)
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _copy              BOOK_COPY%ROWTYPE;
+    _available_copies  BOOK_COPY%ROWTYPE[];
+    _possible_branches branch.ID%TYPE[];
+BEGIN
+
+    IF _preferred_branches IS NULL THEN
+        _possible_branches := ARRAY(SELECT id FROM branch);
+    ELSE
+        _possible_branches := _preferred_branches;
+    END IF;
+
+    _available_copies := ARRAY(
+            SELECT ROW (id, branch, book, removed)
+            FROM book_copy
+            WHERE book = _book
+              AND id NOT IN (SELECT copy
+                             FROM loan
+                             WHERE returned IS NULL)
+              AND removed = FALSE
+                         );
+
+    IF ARRAY_LENGTH(_available_copies, 1) IS NULL THEN
+        RAISE EXCEPTION 'There are no copies available for book %.', _book;
+    END IF;
+
+    FOREACH _copy IN ARRAY _available_copies
+        LOOP
+
+            IF _copy.branch = ANY (_possible_branches) THEN
+                INSERT INTO loan (patron, copy)
+                VALUES (_patron, _copy.id);
+
+                RAISE NOTICE 'Loaned copy % from branch %.',
+                    _copy.id, _copy.branch;
+
+                RETURN;
+            END IF;
+
+        END LOOP;
+
+    RAISE EXCEPTION 'No copies available in branches % for book %.',
+        _preferred_branches, _book;
+END;
+$$;
