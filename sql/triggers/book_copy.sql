0cb3c5f8041341e375ebbe05a653572ce58132c3
Lechemi <micheleceroni0@gmail.com>
2024-10-30 08:30:27 +0100

    loaned copies cannot be updated

--- sql/triggers/book_copy.sql
+++ sql/triggers/book_copy.sql
@@ -16,24 +16,14 @@ CREATE TRIGGER bi_book_copy_set_default_values
     FOR EACH ROW
 EXECUTE PROCEDURE set_default_book_copy_values();
 
-/*
-    'removed' field can only go from FALSE to TRUE.
-    Also, a loaned copy cannot be removed.
- */
+-- 'removed' field can only go from FALSE to TRUE.
 CREATE OR REPLACE FUNCTION book_copy_enforce_removal_policy() RETURNS TRIGGER
     LANGUAGE plpgsql
 AS
 $$
 BEGIN
-    IF new.removed IS DISTINCT FROM old.removed THEN
-        IF new.removed IS FALSE THEN
-            RAISE EXCEPTION 'Removed book copies cannot be restored.';
-        END IF;
-
-        IF new.id IN (SELECT copy FROM loan WHERE returned IS NULL) THEN
-            RAISE EXCEPTION 'Cannot remove a currently loaned copy.';
-        END IF;
-
+    IF new.removed IS DISTINCT FROM old.removed AND new.removed IS FALSE THEN
+        RAISE EXCEPTION 'Removed book copies cannot be restored.';
     END IF;
 
     RETURN new;
@@ -45,6 +35,26 @@ CREATE TRIGGER bu_book_copy_enforce_removal_policy
     ON book_copy
     FOR EACH ROW
 EXECUTE PROCEDURE book_copy_enforce_removal_policy();
+
+-- A loaned copy cannot be updated in any way
+CREATE OR REPLACE FUNCTION deny_update_on_loan() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF old.id IN (SELECT copy FROM loan WHERE returned IS NULL) THEN
+        RAISE EXCEPTION 'Cannot update a currently loaned copy.';
+    END IF;
+
+    RETURN new;
+END;
+$$;
+
+CREATE TRIGGER bu_book_copy_deny_update_on_loan
+    BEFORE UPDATE
+    ON book_copy
+    FOR EACH ROW
+EXECUTE PROCEDURE deny_update_on_loan();
 
 -- Deny deletion of records
 CREATE TRIGGER bd_book_copy_deny_deletion
