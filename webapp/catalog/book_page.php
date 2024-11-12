26057aaf76bafd8c07803561d9f7adc3455cb6e0
Lechemi <micheleceroni0@gmail.com>
2024-11-12 19:04:30 +0100

    loan requests modal for preferred branch

--- webapp/catalog/book_page.php
+++ webapp/catalog/book_page.php
@@ -69,16 +69,59 @@ if (!empty($_GET['isbn'])) {
                 ?>
             </p>
 
-            <form action="" method="POST">
-                <input type="hidden" name="isbn" value="<?php echo htmlspecialchars($isbn); ?>">
-                <button type="submit"
-                        class="btn btn-primary" <?php echo ($bookDetails['available_copies'] <= 0) ? 'disabled' : ''; ?>>
-                    Request Loan
-                </button>
-            </form>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                Request loan
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                 aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Loan request</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form>
+                            <div class="modal-body">
+                                <div class="mb-3">
+                                    <label for="input-datalist" class="form-label">Do you have a preferred branch?</label>
+                                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Insert a location" list="list-timezone" id="input-datalist">
+                                    <datalist id="list-timezone">
+                                        <option selected>No preference</option>
+
+                                        <?php
+
+                                            $branches = ['Lecco', 'Milano', 'Como', 'Bergamo', 'Varese', 'Mandello'];
+                                            foreach ($branches as $branch) {
+                                                echo '<option>' . $branch . '</option>';
+                                            }
+
+                                        ?>
+                                    </datalist>
+                                    <div id="emailHelp" class="form-text">If no preference is specified, a copy can be provided from any branch.</div>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" class="btn btn-primary">Request</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </div>
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', e => {
+        $('#input-datalist').autocomplete()
+    }, false);
+</script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
