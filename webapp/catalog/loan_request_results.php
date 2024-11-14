39259e9b74f3c6e49876d99860ec21907796b69e
Lechemi <micheleceroni0@gmail.com>
2024-11-14 10:49:49 +0100

    boostrap dependencies

--- webapp/catalog/loan_request_results.php
+++ webapp/catalog/loan_request_results.php
@@ -45,8 +45,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['isbn'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loan Request Result</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Loan request results</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body>
 
@@ -59,14 +61,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['isbn'])) {
                 <div class="alert alert-success mt-4" role="alert">
                     <?php
                     print_r($result);
-                    print('BOOK: ' . $_POST['isbn']);
                     ?>
                 </div>
             <?php else: ?>
                 <div class="alert alert-danger mt-4" role="alert">
                     Something went wrong with your loan request.
                     <?php
-                        print($result['error']);
+                    print($result['error']);
                     ?>
                 </div>
             <?php endif; ?>
@@ -78,7 +79,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['isbn'])) {
     </div>
 </div>
 
-<!-- Optional: Bootstrap JS for styling if needed -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 </html>
