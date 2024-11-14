3216564215759c12d25b21883f58737dd284cd19
Lechemi <micheleceroni0@gmail.com>
2024-11-14 12:19:05 +0100

    displaying patron's active loans with a table

--- webapp/patron/manage_loans.php
+++ webapp/patron/manage_loans.php
@@ -27,6 +27,30 @@ $activeLoans = pg_fetch_all($result);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
+
+    <style>
+        /* Basic table styling */
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+
+    <!-- Add some CSS to style the ISBN -->
+    <style>
+        .isbn {
+            font-size: 0.8em; /* Smaller font size */
+            color: #888; /* Lighter color */
+        }
+    </style>
+
 </head>
 
 <body>
@@ -35,36 +59,53 @@ $activeLoans = pg_fetch_all($result);
     <?php include 'navbar.php'; ?>
 </div>
 
-<div class="container my-4">
-    <?php foreach ($activeLoans as $loan): ?>
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo htmlspecialchars($loan['title']); ?></h5>
-                <h6 class="card-subtitle mb-2 text-muted">ISBN: <?php echo htmlspecialchars($loan['isbn']); ?></h6>
 
-                <p class="card-text">
-                    <strong>Branch:</strong> <?php echo $loan['address'] . ' - ' . $loan['city']; ?><br>
-                    <strong>Loan started:</strong> <?php
-                    try {
-                        $start = new DateTime($loan['start']);
-                        echo $start->format('Y-m-d H:i:s');
-                    } catch (DateMalformedStringException $e) {
-                        echo 'Some error occurred';
-                    }
-                    ?><br>
-                    <strong>You have to return this book by:</strong> <?php
-                    try {
-                        $due = new DateTime($loan['due']);
-                        echo $due->format('Y-m-d H:i:s');
-                    } catch (DateMalformedStringException $e) {
-                        echo 'Some error occurred';
-                    }
-                    ?><br>
-                </p>
-            </div>
-        </div>
-    <?php endforeach; ?>
+
+<div class="container my-4">
+
+
+    <table>
+        <thead>
+        <tr>
+            <th>Book</th>
+            <th>Branch</th>
+            <th>Start</th>
+            <th>Due</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        // Loop through each row of data
+        foreach ($activeLoans as $loan) {
+            try {
+                $start = new DateTime($loan['start']);
+                $due = new DateTime($loan['due']);
+                $start = $start->format('Y-m-d H:i:s');
+                $due = $due->format('Y-m-d H:i:s');
+            } catch (DateMalformedStringException $e) {
+                echo 'Some error occurred';
+            }
+
+            $branch = $loan['address'] . ' - ' . $loan['city'];
+
+            // Include ISBN next to the title
+            $isbn = $loan['isbn'];
+            $titleWithIsbn = "{$loan['title']} <span class='isbn'>{$isbn}</span>";
+
+            echo "<tr>";
+            echo "<td>{$titleWithIsbn}</td>";
+            echo "<td>{$branch}</td>";
+            echo "<td>{$start}</td>";
+            echo "<td>{$due}</td>";
+            echo "</tr>";
+        }
+        ?>
+        </tbody>
+    </table>
+
+
 </div>
+
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
