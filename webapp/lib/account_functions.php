5c1ac680d1bbdabcdd71734574364def20d476d7
Lechemi <micheleceroni0@gmail.com>
2024-11-18 15:21:48 +0100

    get_patron retrieves patron-related info

---
+++ webapp/lib/account_functions.php
@@ -0,0 +1,23 @@
+<?php
+
+use PgSql\Result;
+
+include_once('../lib/connection.php');
+
+/*
+ * Retrieves the patron associated with userId.
+ */
+function get_patron($userId): Result|false
+{
+    $db = open_connection();
+    $sql = "
+        SELECT *
+        FROM library.patron
+        WHERE patron.user = '$userId'
+    ";
+
+    pg_prepare($db, 'patron', $sql);
+    $result = pg_execute($db, 'patron', array());
+    close_connection($db);
+    return $result;
+}
