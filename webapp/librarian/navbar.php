0ea64d1ef52582e50d6d2a5bb5688e0d1e409d5d
Lechemi <micheleceroni0@gmail.com>
2024-12-09 16:09:26 +0100

    added navbar to book page

--- webapp/librarian/navbar.php
+++ webapp/librarian/navbar.php
@@ -26,7 +26,7 @@ $current_page = basename($_SERVER['PHP_SELF']); // Name of the current file
             <!-- Left-aligned links -->
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link <?php echo ($current_page == 'catalog.php') ? 'active' : ''; ?>"
+                    <a class="nav-link <?php echo (in_array($current_page, ['catalog.php', 'book.php'])) ? 'active' : ''; ?>"
                        href="../catalog/catalog.php">Book catalog</a>
                 </li>
                 <li class="nav-item">
