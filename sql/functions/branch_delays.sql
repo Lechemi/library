998db5bb1afe16c6f68b5e799c0c2b4b628bb54b
Lechemi <micheleceroni0@gmail.com>
2024-12-09 15:50:52 +0100

    delays function retrieves due date too

--- sql/functions/branch_delays.sql
+++ sql/functions/branch_delays.sql
@@ -1,20 +1,21 @@
 /*
     Given a branch, returns a table containing expired loans
-    and the corresponding readers.
+    and the corresponding patrons.
  */
 CREATE OR REPLACE FUNCTION delays(my_branch branch.ID%TYPE)
     RETURNS TABLE
             (
                 PATRON patron.USER%TYPE,
                 BOOK   book.ISBN%TYPE,
-                COPY   book_copy.ID%TYPE
+                COPY   book_copy.ID%TYPE,
+                DUE    loan.DUE%TYPE
             )
     LANGUAGE plpgsql
 AS
 $$
 BEGIN
     RETURN QUERY
-        SELECT loan.patron AS patron, bc.book AS book, bc.id AS copy
+        SELECT loan.patron AS patron, bc.book AS book, bc.id AS copy, loan.due as due
         FROM loan
                  INNER JOIN library.book_copy bc ON bc.id = loan.copy
         WHERE returned IS NULL
