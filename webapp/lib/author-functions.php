759faccc9c38dec1665ce49d799f32c77901de89
Lechemi <micheleceroni0@gmail.com>
2025-01-02 15:46:29 +0100

    add_author function

--- webapp/lib/author-functions.php
+++ webapp/lib/author-functions.php
@@ -23,7 +23,34 @@ function get_author($id): false|Result
     return $result;
 }
 
-function add_author($firstName, $lastName, $bio, )
+/*
+ * Adds a new author to the catalog.
+ */
+/**
+ * @throws Exception
+ */
+function add_author($firstName, $lastName, $bio, $birthDate, $deathDate, $alive): void
 {
+    $db = open_connection();
 
+    if ($deathDate) {
+        $sql = "
+        INSERT INTO library.author (first_name, last_name, bio, birth_date, death_date, alive)
+        VALUES ('$firstName', '$lastName', '$bio', '$birthDate', '$deathDate', '$alive')
+        ";
+    } else {
+        $sql = "
+        INSERT INTO library.author (first_name, last_name, bio, birth_date, alive)
+        VALUES ('$firstName', '$lastName', '$bio', '$birthDate', '$alive')
+        ";
+    }
+
+    pg_prepare($db, 'add-author', $sql);
+    $result = pg_execute($db, 'add-author', array());
+
+    if (!$result) {
+        throw new Exception('Cannot insert author. ' . pg_last_error($db));
+    }
+
+    close_connection($db);
 }
