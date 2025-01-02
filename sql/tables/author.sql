97f97fddfd24d6920d70c3a7a127f7e504c31d3b
Lechemi <micheleceroni0@gmail.com>
2025-01-02 19:06:47 +0100

    added constraint for uniqueness on author.sql

--- sql/tables/author.sql
+++ sql/tables/author.sql
@@ -1,18 +1,23 @@
-CREATE TABLE author
+create table author
 (
-    id         SERIAL
-        PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL
-        CHECK ((first_name)::TEXT ~* '^.+$'::TEXT),
-    last_name  VARCHAR(100) NOT NULL
-        CHECK ((last_name)::TEXT ~* '^.+$'::TEXT),
-    bio        TEXT         NOT NULL
-        CHECK (bio ~* '^.+$'::TEXT),
-    birth_date DATE,
-    death_date DATE,
-    alive      BOOLEAN      NOT NULL,
-    CONSTRAINT check_death_date_after_birth_date
-        CHECK ((death_date IS NULL) OR (birth_date IS NULL) OR (death_date > birth_date)),
-    CONSTRAINT check_alive_false_if_dead
-        CHECK ((death_date IS NULL) OR (alive = FALSE))
+    id         serial
+        primary key,
+    first_name varchar(100) not null
+        constraint author_first_name_check
+            check ((first_name)::text ~* '^.+$'::text),
+    last_name  varchar(100) not null
+        constraint author_last_name_check
+            check ((last_name)::text ~* '^.+$'::text),
+    bio        text         not null
+        constraint author_bio_check
+            check (bio ~* '^.+$'::text),
+    birth_date date,
+    death_date date,
+    alive      boolean      not null,
+    constraint unique_author_identity
+        unique (first_name, last_name, birth_date),
+    constraint check_death_date_after_birth_date
+        check ((death_date IS NULL) OR (birth_date IS NULL) OR (death_date > birth_date)),
+    constraint check_alive_false_if_dead
+        check ((death_date IS NULL) OR (alive = false))
 );
