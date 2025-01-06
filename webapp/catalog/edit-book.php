1e2320bd627cba78d4af863078df94a037039d14
Lechemi <micheleceroni0@gmail.com>
2025-01-06 12:50:27 +0100

    form for editing books works

--- webapp/catalog/edit-book.php
+++ webapp/catalog/edit-book.php
@@ -20,8 +20,10 @@ if (!empty($_GET['isbn'])) {
 
     $bookDetails = group_authors($result)[$isbn];
     $authorString = '';
+    $authorList = [];
     foreach ($bookDetails['authors'] as $author) {
         $authorString .= $author['id'] . ', ';
+        $authorList[] = $author['id'];
     }
 
     $authorString = substr($authorString, 0, -2);
@@ -30,6 +32,33 @@ if (!empty($_GET['isbn'])) {
 } else {
     echo "Error, no book.";
     exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $title = $_POST['title'] != $bookDetails['title'] ? $_POST['title'] : null;
+    $publisher = $_POST['publisher'] != $bookDetails['publisher'] ? $_POST['publisher'] : null;
+    $blurb = $_POST['blurb'] != $bookDetails['blurb'] ? $_POST['blurb'] : null;
+
+    if ($title != null or $publisher != null or $blurb != null) {
+        try {
+            update_book($isbn, $title, $blurb, $publisher);
+        } catch (Exception $e) {
+            print $e->getMessage();
+        }
+    }
+
+    $authors = array_map('trim', explode(',', $_POST['authors']));
+    $authors = array_filter($authors, 'is_numeric');
+
+    if (array_diff($authors, $authorList) || array_diff($authorList, $authors)) {
+        try {
+            update_authors($isbn, $authors);
+        } catch (Exception $e) {
+            print $e->getMessage();
+        }
+    }
+
 }
 
 ?>
@@ -56,18 +85,18 @@ if (!empty($_GET['isbn'])) {
 <div class="container my-4">
     <h5 class="mb-4"> Editing book <?php echo $isbn ?></h5>
 
-    <form>
+    <form method="POST" action="">
 
         <!-- Title -->
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" value="<?php echo $bookDetails['title'] ?>">
+            <input type="text" name="title" class="form-control" id="title" value="<?php echo $bookDetails['title'] ?>">
         </div>
 
         <!-- Publisher -->
         <div class="mb-3">
             <label for="publisher" class="form-label">Publisher</label>
-            <select id="publisher" class="form-select">
+            <select id="publisher" name="publisher" class="form-select">
                 <option selected> <?php echo $bookDetails['publisher'] ?> </option>
 
                 <?php
@@ -88,7 +117,7 @@ if (!empty($_GET['isbn'])) {
         <!-- Author(s) -->
         <div class="mb-3">
             <label for="authors" class="form-label">Author(s)</label>
-            <input type="text" class="form-control" id="authors" value="<?php echo $authorString ?>"
+            <input type="text" name="authors" class="form-control" id="authors" value="<?php echo $authorString ?>"
                    aria-describedby="authorsHelp">
             <div id="authorsHelp" class="form-text">
                 Insert author id's separated by commas (e.g. 123, 456, 789). Spaces are ignored.
@@ -98,8 +127,10 @@ if (!empty($_GET['isbn'])) {
         <!-- Blurb -->
         <div class="mb-3">
             <label for="blurb" class="form-label">Blurb</label>
-            <textarea id="blurb" class="form-control"><?php echo $bookDetails['blurb'] ?></textarea>
+            <textarea id="blurb" name="blurb" class="form-control"><?php echo $bookDetails['blurb'] ?></textarea>
         </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
 
     </form>
 
