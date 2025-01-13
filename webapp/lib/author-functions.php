a9e7f860731c1c49f039962ba227d0b7f3759128
Lechemi <micheleceroni0@gmail.com>
2025-01-13 10:38:00 +0100

    improved add_author

--- webapp/lib/author-functions.php
+++ webapp/lib/author-functions.php
@@ -10,22 +10,17 @@ include_once('../lib/connection.php');
  */
 function add_author($firstName, $lastName, $alive, $bio, $birthDate = null, $deathDate = null): void
 {
-    // Ensure required fields are set
-    if (!$firstName || !$lastName || !$bio || !isset($alive)) {
+
+    if (!$firstName || !$lastName || !$bio || !isset($alive))
         throw new InvalidArgumentException('Missing required fields.');
-    }
 
     $alive = $alive ? 1 : 0;
 
-    $db = open_connection();
-
-    // Start building the SQL query
     $columns = ['first_name', 'last_name', 'alive', 'bio'];
     $values = ['$1', '$2', '$3', '$4'];
     $params = [$firstName, $lastName, $alive, $bio];
     $paramIndex = 5;
 
-    // Add optional fields if they are set
     if (!empty($birthDate)) {
         $columns[] = 'birth_date';
         $values[] = '$' . $paramIndex++;
@@ -37,19 +32,16 @@ function add_author($firstName, $lastName, $alive, $bio, $birthDate = null, $dea
         $params[] = $deathDate;
     }
 
-    // Construct the final SQL query
     $sql = "
         INSERT INTO library.author (" . implode(', ', $columns) . ")
         VALUES (" . implode(', ', $values) . ")
     ";
 
-    // Prepare and execute the query
+    $db = open_connection();
     pg_prepare($db, 'add-author', $sql);
     $result = pg_execute($db, 'add-author', $params);
 
-    if (!$result) {
-        throw new Exception('Cannot insert author. ' . pg_last_error($db));
-    }
+    if (!$result) throw new Exception(prettifyExceptionMessages(pg_last_error($db)));
 
     close_connection($db);
 }
