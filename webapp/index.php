87d16f77f34bcb4c0feeb12004f491c8cb46609c
Lechemi <micheleceroni0@gmail.com>
2025-01-18 14:23:29 +0100

    login functionalities are all in index.php

--- webapp/index.php
+++ webapp/index.php
@@ -1,7 +1,26 @@
 <?php
 ini_set('display_errors', 'On');
 ini_set('error_reporting', E_ALL);
+include_once('lib/redirect.php');
+include_once('lib/account-functions.php');
 session_start();
+
+$result = null;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['usr'];
+    $psw = $_POST['psw'];
+
+    $user = [];
+
+    try {
+        $user = authenticate($email, $psw);
+        $_SESSION['user'] = $user;
+        redirect('catalog/catalog.php');
+    } catch (Exception $e) {
+        $result = ['ok' => false, 'msg' => $e->getMessage()];
+    }
+}
+
 ?>
 
 <!doctype html>
@@ -9,7 +28,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Library</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
@@ -32,7 +51,7 @@ session_start();
 
             <h2><strong>Login</strong></h2>
 
-            <form class="" method="POST" action="lib/login.php">
+            <form method="POST" action="">
                 <div class="mb-3">
                     <label for="inputEmail" class="form-label">Email</label>
                     <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp"
@@ -45,20 +64,13 @@ session_start();
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
 
-            <?php
-            if (isset($_SESSION['login_error'])) {
-
-                ?>
-                <div class="alert alert-danger d-flex align-items-center" role="alert">
-                    <i class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2"></i>
-                    <div>
-                        <?php echo $_SESSION['login_error'] ?>
-                    </div>
+            <?php if ($result): ?>
+                <div class="alert <?= $result['ok'] ? 'alert-success' : 'alert-danger' ?> alert-dismissible fade show mt-3"
+                     role="alert">
+                    <?php echo htmlspecialchars($result['msg']); ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
-                <?php
-
-            }
-            ?>
+            <?php endif; ?>
         </div>
     </div>
 </div>
