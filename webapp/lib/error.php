65b1d987e09a2d13eaeddfaa407c64c1c04c0c7f
Lechemi <micheleceroni0@gmail.com>
2025-02-09 14:10:50 +0100

    error page title

--- webapp/lib/error.php
+++ webapp/lib/error.php
@@ -7,7 +7,7 @@ include_once('../lib/redirect.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit book</title>
+    <title>Error</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
